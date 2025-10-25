91308c1f899ba2c52b6615723f0f6673628f94b8
Rizal Ilham <rzlilhm09@gmail.com>
2025-10-25 12:41:12 +0700

    push fundraiser api

--- src/routes/fundraiser.routes.js
+++ src/routes/fundraiser.routes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const FundraiserController = require('../controllers/fundraiser.controller');
 
 router.post('/leaderboards', FundraiserController.list);
-router.post('/', authenticate, BookController.create);
 
 module.exports = router;
 
